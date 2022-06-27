1f884411909f2e16488f34ae20fdf4e762c16f50
Paxton Edgar <paxtonedgar3@gmail.com>
2022-06-27 13:27:33 -0400

    init push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.util.*; 
 public class Main {
     public static void main(String[] args) {
-        // your code here
+    Scanner sc = new Scanner(System.in); 
+    ArrayList<String> people = new ArrayList<String>();
+    
+    for(int i=0; i<10; i++){
+        System.out.println("Enter a name"); 
+        String name = sc.nextLine();
+        name = name.toString(); 
+        people.add(name);   
+    }
+    System.out.println(); 
+    people.forEach(System.out::println);
+
+    
     }
 }
